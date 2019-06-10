6ef10470d598e01cb9909fe74d659deb9e7389c9
Farahat <m.farahat@maxab.io>
2019-06-10 11:19:27 +0200

    handle unit test example

--- app/src/test/java/com/itsmart/baseproject/ExampleUnitTest.kt
+++ app/src/test/java/com/itsmart/baseproject/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.itsmart.baseproject
 
+import com.itsmart.helpers.inputValidation.isValidPassword
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
